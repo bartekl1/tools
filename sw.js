0843f2465d4b022fda6f70e342a5895ea60a7509
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-09-21 20:15:21 +0200

    Website works offline

---
+++ sw.js
@@ -0,0 +1,41 @@
+const version = "1.0";
+// Choose a cache name
+const cacheName = `cache-tools-v${version}`;
+// List the files to precache
+const precacheResources = [
+    "/tools/",
+    "/tools/manifest.json",
+    "/tools/css/style.css",
+    "/tools/img/icons/elephant.ico",
+    "/tools/img/icons/elephant.png",
+    "/tools/js/theme.js",
+    "/tools/js/translate.js",
+    "/tools/js/index.js",
+];
+
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener("install", (event) => {
+    console.log("Service worker install event!");
+
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => cache.addAll(precacheResources))
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    console.log("Service worker activate event!");
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener("fetch", (event) => {
+    console.log("Fetch intercepted for:", event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
