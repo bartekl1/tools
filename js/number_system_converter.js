4820a5078f425f5f6d480ccaa7785b9548342058
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-10-09 18:21:55 +0200

    Added copy button to number system converter

--- js/number_system_converter.js
+++ js/number_system_converter.js
@@ -35,3 +35,21 @@ document.querySelectorAll(".form-control").forEach((e) => {
         });
     });
 });
+
+document.querySelectorAll(".copy").forEach((e) => {
+    e.addEventListener("click", (evt) => {
+        navigator.clipboard.writeText(
+            evt.currentTarget.parentElement.querySelector("input").value
+        );
+
+        evt.currentTarget.innerHTML = '<i class="bi bi-clipboard-check"></i>';
+
+        setTimeout(
+            (el) => {
+                el.innerHTML = '<i class="bi bi-clipboard"></i>';
+            },
+            2000,
+            evt.currentTarget
+        );
+    });
+});
