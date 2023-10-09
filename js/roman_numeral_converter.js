ca28052ea33727d2f2b34e61dbb80da1bd2e7d33
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-10-09 18:20:07 +0200

    Added copy button to roman numeral converter

--- js/roman_numeral_converter.js
+++ js/roman_numeral_converter.js
@@ -59,3 +59,21 @@ document.querySelector("#roman").addEventListener("change", () => {
         document.querySelector("#arabic").value = arabic;
     }
 });
+
+document.querySelectorAll(".copy").forEach((e) => {
+    e.addEventListener("click", (evt) => {
+        navigator.clipboard.writeText(
+            evt.currentTarget.parentElement.querySelector("input").value
+        );
+
+        evt.currentTarget.innerHTML = '<i class="bi bi-clipboard-check"></i>';
+
+        setTimeout(
+            (el) => {
+                el.innerHTML = '<i class="bi bi-clipboard"></i>';
+            },
+            2000,
+            evt.currentTarget
+        );
+    });
+});
